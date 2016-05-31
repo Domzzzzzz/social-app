4e81bc2da6e0d977761c06bc52ea93c07c5a51d3
Dominic <dom42486@gmail.com>
2016-05-31 12:50:39 -0700

    set up user authentication thru signup form

--- client/components/features/featurelist.jsx
+++ client/components/features/featurelist.jsx
@@ -7,7 +7,7 @@ Featurelist = React.createClass({
     {icon:"fa fa-image fa-2x",bigtext:" See pics & updates",littletext:"your friends & family are posting."},
     {icon:"fa fa-share-alt fa-2x",bigtext:" Share what\'s on your mind",littletext:"with your friends & family."},
     {icon:"fa fa-group fa-2x",bigtext:" Meet new people",littletext:"& make friends from all over the world."},
-    {icon:"fa fa-search fa-2x",bigtext:" Find everything you need",littletext:"with our awesome search feature"}
+    {icon:"fa fa-search fa-2x",bigtext:" Find everything you need",littletext:"with our awesome search feature."}
   ],
   render(){
     var rows = this.features.map(function(feature){
@@ -26,7 +26,7 @@ Featurelist = React.createClass({
     return(
       <div>
         <h2 className="col-md-11 featurelist hidden-xs">
-          Stay connected with friends, family, & the rest of the world on Social Meet!
+          Social Meet - a place to connect with family, friends, & people from all over the world!
         </h2>
         <ul className="ds-btn hidden-xs">
           {rows}
--- client/components/signup/signupform.jsx
+++ client/components/signup/signupform.jsx
@@ -1,4 +1,52 @@
+// mixin makes data available to Meteor accessible by React
+// create error message for failed registration
+// handle form submissions with onSubmit()
+// create user object and params for storing data into DB
+// set up redirect to dashboard after a user is created
 Signupform = React.createClass({
+  mixins:[ReactMeteorData],
+  getMeteorData(){
+    let data = {};
+    data.currentUser = Meteor.user();
+    return data;
+  },
+  getInitialState(){
+    return {
+      message:'',
+      messageClass:''
+    }
+  },
+  displayError(message){
+    this.setState({message:message, messageClass:'alert alert-danger registerError'});
+  },
+  handleSubmit(e){
+    e.preventDefault();
+    this.setState({message:'',messageClass:'hidden'});
+    var that = this;
+    var first_name = ReactDOM.findDOMNode(this.refs.first_name).value.trim();
+    var last_name = ReactDOM.findDOMNode(this.refs.last_name).value.trim();
+    var email = ReactDOM.findDOMNode(this.refs.email).value.trim();
+    var password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+    var user = {
+      email: email,
+      password: password,
+      profile: {
+        fullname:(first_name + last_name).toLowerCase(),
+        firstname: first_name,
+        lastname: last_name,
+        avatar:'http://placehold.it/150x150',
+        friends:[]
+      }
+    };
+    Accounts.createUser(user,function(e){
+      if(e){
+        that.displayError(e.reason);
+      }
+      else {
+        FlowRouter.go('/dashboard');
+      }
+    })
+  },
   render(){
     return(
       <div className="row">
@@ -6,7 +54,7 @@ Signupform = React.createClass({
           <h1>Sign Up</h1>
           <p className="text-muted">It's free to sign up!</p>
         </div>
-        <form>
+        <form role="form" onSubmit={this.handleSubmit}>
           <div className="col-sm-9">
             <div className="row">
               <div className="col-sm-6 form-group">
@@ -23,6 +71,7 @@ Signupform = React.createClass({
               <input placeholder="Password" type="password" ref="password" className="form-control"></input>
             </div>
             <button type="submit" className="btn btn-md btn-success">Sign Up</button>
+            <span className={this.state.messageClass}>{this.state.message}</span>
           </div>
         </form>
       </div>
